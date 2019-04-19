ab0da41e2f763c7f01c88b8b263778c963f423a5
Emily DeWitt <emilykdewitt@gmail.com>
2019-04-19 15:59:27 -0500

    Added bootstrap card formatting and other fave place data to cards

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-//XHR stands for XML HTTP Request
-
 const printToDom = (divId, textToPrint) => {
     const selectedDiv = document.getElementById(divId);
     selectedDiv.innerHTML = textToPrint;
@@ -12,10 +10,13 @@ const domStringBuilder = (array) => {
     domString += `<div class="container">`;
     domString +=    `<div class="row">`;
     array.forEach((place) => {
-        domString += `<div class="col-4">`;
-        domString +=    `<div class="placeCard">`;
-        domString +=        `<img class="placeImage" src="${place.cityImage}">`;
-        domString +=        `<h3>${place.cityName}, ${place.cityState}`;
+        domString += `<div class="col col-9 col-sm-9 col-md-6 col-lg-4 d-flex align-items-stretch">`;
+        domString +=    `<div class="placeCard shadow p-3 mb-5 bg-white rounded">`;
+        domString +=        `<img class="placeImage" src="${place.cityImage}"></br>`;
+        domString +=        `<h4>${place.cityName}, ${place.cityState}</h4>`;
+        domString +=        `<p>Favorite bar: ${place.favoriteBar}</p>`;
+        domString +=        `<p>Favorite restaurant: ${place.favoriteRestaurant}</p>`;
+        domString +=        `<p>Favorite tourist attraction: ${place.favoriteTouristAttraction}</p>`;
         domString +=    `</div>`;
         domString +=  `</div>`;
     })
@@ -24,7 +25,7 @@ const domStringBuilder = (array) => {
     printToDom('placeCards', domString);
 };
 
-function executeThisCodeAfterFileLoads(){ //DON'T USE A FAT ARROW!
+function executeThisCodeAfterFileLoads(){
     const data = JSON.parse(this.responseText);
     places = data.favoritePlaces;
     console.log('dataLoad 1 OK');
